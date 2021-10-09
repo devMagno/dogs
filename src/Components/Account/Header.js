8a1ed741b1d2785ca7c6641390462fa37db1f2b9
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-10-08 23:50:35 -0300

    Add user page header

--- src/Components/Account/Header.js
+++ src/Components/Account/Header.js
@@ -1,9 +1,25 @@
 import { HeaderNav } from './HeaderNav'
+import styles from './Header.module.css'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router'
 
 export function Header() {
+  const [title, setTitle] = useState('')
+  const location = useLocation()
+  
+  useEffect(() => {
+    setTitle(location.pathname)
+    if (location.pathname === "/conta/estatisticas")
+      setTitle("Estatísticas")
+    else if (location.pathname === "/conta/postar")
+      setTitle("Publicar foto")
+    else 
+      setTitle("Minha conta")
+  }, [location])
+
   return (
-    <header>
-      <h1 className="title">Título</h1>
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
       <HeaderNav />
     </header>
   )
--- src/Components/Account/HeaderNav.js
+++ src/Components/Account/HeaderNav.js
@@ -1,32 +1,62 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { UserContext } from '../../UserContext'
 import { ReactComponent as MyPhotos } from '../../Assets/feed.svg'
 import { ReactComponent as Statistics } from '../../Assets/estatisticas.svg'
 import { ReactComponent as AddPhoto } from '../../Assets/adicionar.svg'
 import { ReactComponent as Logout } from '../../Assets/sair.svg'
+import { useLocation } from 'react-router'
+import { useMedia } from '../../Hooks/useMedia'
 import styles from './HeaderNav.module.css'
 
 export function HeaderNav() {
   const { logoutUser } = useContext(UserContext)
+  const isMobile = useMedia('(max-width: 40rem)')
+
+  const [isMobileMenuActive, setIsMobileMenuActive] = useState(false)
+
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    setIsMobileMenuActive(false)
+  }, [pathname])
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta">
-        <MyPhotos />
-        Minhas fotos
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <Statistics /> Estatísticas
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <AddPhoto />
-        Adicionar foto
-      </NavLink>
-      <button onClick={logoutUser}>
-        <Logout />
-        Sair
-      </button>
-    </nav>
+    <>
+      {isMobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            isMobileMenuActive && styles.mobileButtonActive
+          }`}
+          onClick={() => {
+            setIsMobileMenuActive(!isMobileMenuActive)
+          }}
+        ></button>
+      )}
+
+      <nav
+        className={`${isMobile ? styles.mobileNav : styles.nav} ${
+          isMobileMenuActive && styles.activeMobileNav
+        }`}
+      >
+        <NavLink to="/conta" end>
+          <MyPhotos />
+          {isMobile && 'Minhas fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Statistics />
+          {isMobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AddPhoto />
+          {isMobile && 'Adicionar foto'}
+        </NavLink>
+        <button onClick={logoutUser}>
+          <Logout />
+          {isMobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   )
 }
